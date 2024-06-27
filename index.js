2160a5bdeaa1c9dc1587b4c69d290238dd17f364
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-06-27 13:49:59 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(a, b){
+    const divide=a/b;
+    return divide
+}
+divide(2000, 100)
+
+function square(x){
+    const square=x*x
+    return square
+}
+square(2)
+function add(a, b){
+    const add=a+b
+    return add 
+}
+add(3, 4)
